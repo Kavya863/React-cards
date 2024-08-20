dc03c2d54996e3d70805357217afadbb04195340
Kavya-JH <hkavyaj@gmail.com>
2024-08-20 20:10:01 +0530

    initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "MS Dhoni",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6hKCHEeFbT_AOBQTttjmyRihpYrd7oPSt5w&s",
+    phone: "+91 7738292012",
+    email: "MS@gmail.com"
+  },
+  {
+    name: "R Jadeja",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfhcRTV-4A7tC7lZeSvrNA-xBjY55Uiur-Koj3KHNAocWdt7p_6dRIlCoZPc5THL0CDE4&usqp=CAU",
+    phone: "+91 7823456372",
+    email: "RJ@gmail.com"
+  },
+  {
+    name: "S Raina",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOCwGueBBT-WD3pdwVyYiRq8b_45Bz3fe3m2Lvlpi7QLfSjW0HzQAuzokwaF6CXDIZa0U&usqp=CAU",
+    phone: "+91 8723456312",
+    email: "SR@gmail.com"
+  }
+];
+
+export default contacts;
